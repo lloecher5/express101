5150c4a76adbbb73128f1ac8dd1d6487acf4a4ea
Luke Loecher <ljloecher@gmail.com>
2021-09-30 21:26:23 -0400

    set up express

---
+++ server.js
@@ -0,0 +1,23 @@
+const http = require("http");
+const express = require("express");
+
+const hostname = "127.0.0.1";
+
+const port = 3000;
+
+const app = express();
+
+const server = http.createServer(app);
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("*", (req, res) => {
+  res.status(404);
+  res.send("Page Not Found");
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
+});
